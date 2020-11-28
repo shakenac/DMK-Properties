07cc99e170120b61b3923f2f54235fa4049d3239
Shakena <shakenacme@gmail.com>
2020-11-28 09:38:30 -0500

    Add media query min-width for desktop hover-dropdown submenu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByTagName('nav')[0];
+const propertiesDropdown = document.getElementsByClassName('properties-dropdown')[0];
+const dropMenu = document.getElementsByClassName('drop-menu')[0];
 
 toggleButton.addEventListener('click', () => {
    navbarLinks.classList.toggle('active');
 })
+propertiesDropdown.addEventListener('click', () => {
+   dropMenu.classList.toggle('active');
+})
+
+
+/*
+   Properties drop down menu:
+   1. display: none; regular dropdown menu to hide it
+   2. JS: eventListener;
+      2a. user clicks 'Properites' nav (.properties-dropdown)
+      2b. drop menu w/ 'Dover' and 'Bronx' will appear underneath properties
+
+      .drop-menu {
+         display: flex;
+         flex-direction: column;
+      }
+*/
